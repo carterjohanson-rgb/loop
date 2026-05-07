bfe6964fefed4cf7cd31f78bdd2c9d456bdc8847
carterjohanson-rgb <carter.johanson@school21.org.uk>
2026-05-07 11:12:41 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,1 @@
-input.onButtonPressed(Button.B, function () {
-    for (let index = 0; index < 5; index++) {
-        music.play(music.stringPlayable("- - - - - - - - ", 120), music.PlaybackMode.UntilDone)
-    }
-})
+
